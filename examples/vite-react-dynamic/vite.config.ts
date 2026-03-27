088a3366d5ef92c1b12aeef25607b74bd3187312
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2026-03-27 17:58:09 +0200

    docs: add example project

---
+++ examples/vite-react-dynamic/vite.config.ts
@@ -0,0 +1,49 @@
+import { fileURLToPath, URL } from "node:url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+  define: {
+    global: "globalThis",
+    process: {
+      env: {},
+      version: "v18.0.0",
+    },
+  },
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ["buffer", "process", "stream", "util"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@liquidium/client": fileURLToPath(
+        new URL("../../packages/client/src/index.ts", import.meta.url)
+      ),
+      "@noble/hashes": fileURLToPath(
+        new URL(
+          "../../node_modules/.pnpm/@noble+hashes@1.8.0/node_modules/@noble/hashes",
+          import.meta.url
+        )
+      ),
+      "@noble/hashes/utils": fileURLToPath(
+        new URL(
+          "../../node_modules/.pnpm/@noble+hashes@1.8.0/node_modules/@noble/hashes/utils.js",
+          import.meta.url
+        )
+      ),
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@liquidium/client"],
+  },
+});
