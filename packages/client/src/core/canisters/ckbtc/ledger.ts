efa10e8cbf635dc27b1d49b1aee1c78fcf939837
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2026-07-08 15:50:45 +0200

    refactor(client): delegate ckbtc ledger actor

--- packages/client/src/core/canisters/ckbtc/ledger.ts
+++ packages/client/src/core/canisters/ckbtc/ledger.ts
@@ -1,32 +1,15 @@
-import { Actor, type ActorMethod } from "@icp-sdk/core/agent";
-import type { IDL } from "@icp-sdk/core/candid";
 import { CK_CANISTER_IDS } from "../../config";
-import { LiquidiumError, LiquidiumErrorCode } from "../../errors";
 import type { CanisterContext } from "../../transports/canister-context";
+import { createIcrcLedgerActor, type IcrcLedgerActor } from "../icrc/ledger";
 
-export interface CkBtcLedgerActor {
-  icrc1_fee: ActorMethod<[], bigint>;
-}
-
-const idlFactory: IDL.InterfaceFactory = ({ IDL }) =>
-  IDL.Service({
-    icrc1_fee: IDL.Func([], [IDL.Nat], ["query"]),
-  });
+export type CkBtcLedgerActor = IcrcLedgerActor;
 
 export function createCkBtcLedgerActor(
   canisterContext: CanisterContext
 ): CkBtcLedgerActor {
-  const canisterId = CK_CANISTER_IDS.ckBTC.ledger;
-
-  if (!canisterId) {
-    throw new LiquidiumError(
-      LiquidiumErrorCode.SERVICE_UNAVAILABLE,
-      "ckBTC ledger canister ID is not configured"
-    );
-  }
-
-  return Actor.createActor<CkBtcLedgerActor>(idlFactory, {
-    agent: canisterContext.agent,
-    canisterId,
+  return createIcrcLedgerActor({
+    canisterContext,
+    canisterId: CK_CANISTER_IDS.ckBTC.ledger,
+    ledgerName: "ckBTC",
   });
 }
